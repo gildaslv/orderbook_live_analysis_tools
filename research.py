b38168dce88f06e0503658b4feafb292b6ba4a5a
jeyanderson <jey_andersonn@protonmail.com>
2022-11-15 14:35:33 +0100

    cleaning code

--- research.py
+++ research.py
@@ -6,24 +6,24 @@ async def main():
     import numpy as np
     
     # generating random data values
-    x = np.linspace(1, 100, 100)
-    y = np.linspace(.1,.9,100)
-    y2 = np.linspace(.1,.9,100)
-    y3 = np.linspace(.1,.9,100)
-    y4 = np.linspace(1250,1300,100)
+    x=np.linspace(1,100,100)
+    y=np.linspace(.1,.9,100)
+    y2=np.linspace(.1,.9,100)
+    y3=np.linspace(.1,.9,100)
+    y4=np.linspace(1250,1300,100)
     
     # enable interactive mode
     plt.ion()
     
     # creating subplot and figure
-    fig = plt.figure()
-    ax = fig.add_subplot(221)
-    line1, = ax.plot(x, y,label='40')
-    line2,= ax.plot(x,y2,label='250')
+    fig=plt.figure()
+    ax=fig.add_subplot(221)
+    line1,=ax.plot(x,y,label='40')
+    line2,=ax.plot(x,y2,label='250')
     line3,=ax.plot(x,y3,label='500')
     ax.legend(loc='upper left')
-    ax2 = fig.add_subplot(222)
-    line4, = ax2.plot(x, y4)
+    ax2=fig.add_subplot(222)
+    line4,=ax2.plot(x, y4)
     
     ax.set_xlabel('time')
     ax2.set_xlabel('time')
@@ -33,16 +33,16 @@ async def main():
     while True:
         orderbook = await exchange.watch_order_book('ETHUSDT')
         # print(orderbook['asks'][0], orderbook['bids'][0])
-        bid=sum(x[1] for x in orderbook['bids'][:40])
-        ask=sum(x[1] for x in orderbook['asks'][:40])
+        bid=sum(x[1]for x in orderbook['bids'][:40])
+        ask=sum(x[1]for x in orderbook['asks'][:40])
         y=np.append(y,bid/(bid+ask))
         y=y[-100:]
-        bid=sum(x[1] for x in orderbook['bids'][:250])
-        ask=sum(x[1] for x in orderbook['asks'][:250])
+        bid=sum(x[1]for x in orderbook['bids'][:250])
+        ask=sum(x[1]for x in orderbook['asks'][:250])
         y2=np.append(y2,bid/(bid+ask))
         y2=y2[-100:]
-        bid=sum(x[1] for x in orderbook['bids'][:500])
-        ask=sum(x[1] for x in orderbook['asks'][:500])
+        bid=sum(x[1]for x in orderbook['bids'][:500])
+        ask=sum(x[1]for x in orderbook['asks'][:500])
         y3=np.append(y3,bid/(bid+ask))
         y3=y3[-100:]
         y4=np.append(y4,orderbook['bids'][0][0])
